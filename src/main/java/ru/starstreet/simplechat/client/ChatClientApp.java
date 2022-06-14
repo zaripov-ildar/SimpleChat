e1ddaa24624ae3000bce4352954f5a98215b8a1e
Ildar Zaripov <zariil@yandex.ru>
2022-06-14 23:11:11 +0500

    changed size of window

--- src/main/java/ru/starstreet/simplechat/client/ChatClientApp.java
+++ src/main/java/ru/starstreet/simplechat/client/ChatClientApp.java
@@ -15,7 +15,7 @@ public class ChatClientApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChatClientApp.class.getResource("client-View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 390, 240);
         stage.setTitle("Chat Client");
         stage.setScene(scene);
         stage.show();
